ceba38c3bcd856f346095be38b7760b92a7b2a30
Giordy Pavel Henao Montes <giordy.henao@correounivalle.edu.co>
2024-02-06 10:21:04 -0500

    schema

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+//Configuracion general
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+//API
+app.use('/api', require('./routes/api'));
+
 app.listen(PORT, () =>{
     console.log(`Server Listening on port ${PORT}`)
 })
---
+++ routes/api/estudiantes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) =>{
+
+});
+
+router.get('/:estudianteId', (req, res) =>{
+
+});
+module.exports = router;
